e18e5949c705b7c7080c3e0363d180546ed5031c
roberto <robertomodorcea1@gmail.com>
2023-04-29 16:34:59 +0300

    Protecting API Routes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/api',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
